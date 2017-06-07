9d6d4a9e5740191276659be8b540151a6ca28a99
xrwang <33010701@qq.com>
2017-06-07 15:45:09 +0800

    init

---
+++ DiningPhilosophers/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.nud_numPhils = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lv_info = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.nud_Delay = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_numPhils)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_Delay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Start.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Start.Location = new System.Drawing.Point(336, 14);
+            this.btn_Start.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(407, 106);
+            this.btn_Start.TabIndex = 0;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // nud_numPhils
+            // 
+            this.nud_numPhils.Location = new System.Drawing.Point(208, 30);
+            this.nud_numPhils.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.nud_numPhils.Name = "nud_numPhils";
+            this.nud_numPhils.Size = new System.Drawing.Size(59, 25);
+            this.nud_numPhils.TabIndex = 1;
+            this.nud_numPhils.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Number of Philosophers";
+            // 
+            // lv_info
+            // 
+            this.lv_info.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.lv_info.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.lv_info.FullRowSelect = true;
+            this.lv_info.Location = new System.Drawing.Point(16, 127);
+            this.lv_info.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.lv_info.Name = "lv_info";
+            this.lv_info.Size = new System.Drawing.Size(725, 161);
+            this.lv_info.TabIndex = 3;
+            this.lv_info.UseCompatibleStateImageBehavior = false;
+            this.lv_info.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Philosopher";
+            this.columnHeader1.Width = 70;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Status";
+            this.columnHeader2.Width = 102;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Forks";
+            this.columnHeader3.Width = 215;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "# Meals";
+            this.columnHeader4.Width = 55;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.nud_Delay);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.nud_numPhils);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(16, 6);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox1.Size = new System.Drawing.Size(312, 114);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Setup";
+            // 
+            // nud_Delay
+            // 
+            this.nud_Delay.DecimalPlaces = 1;
+            this.nud_Delay.Increment = new decimal(new int[] {
+            2,
+            0,
+            0,
+            65536});
+            this.nud_Delay.Location = new System.Drawing.Point(208, 78);
+            this.nud_Delay.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.nud_Delay.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nud_Delay.Name = "nud_Delay";
+            this.nud_Delay.Size = new System.Drawing.Size(59, 25);
+            this.nud_Delay.TabIndex = 6;
+            this.nud_Delay.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(153, 80);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Delay";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 312);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lv_info);
+            this.Controls.Add(this.btn_Start);
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MinimumSize = new System.Drawing.Size(18, 339);
+            this.Name = "Form1";
+            this.Text = "Dining Philosophers";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.nud_numPhils)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_Delay)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.NumericUpDown nud_numPhils;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView lv_info;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown nud_Delay;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+
+    }
+}
+
---
+++ DiningPhilosophers/Form1.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            //this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.interruppt);
+        }
+
+
+
+        private int numPhils;
+        private Fork[] forks;
+        private Philosopher[] philosophers;
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            switch (btn_Start.Text)
+            {
+                case "Start":
+                    lv_info.Items.Clear();
+                    btn_Start.Text = "Stop";
+
+                    numPhils = (int)nud_numPhils.Value;
+                    forks = new Fork[numPhils];
+                    philosophers = new Philosopher[numPhils];
+
+                    //create forks
+                    for (int i = 0; i < numPhils; i++)
+                        forks[i] = new Fork(i);
+
+
+                    //create philosophers and start dining
+                    for (int i = 0; i < numPhils; i++)
+                    {
+                        int rightforkID = i - 1;
+                        if (i == 0)
+                            rightforkID = numPhils - 1;
+
+                        philosophers[i] = new Philosopher(i, forks[i], forks[rightforkID], ref lv_info);
+
+                        ListViewItem lvi = new ListViewItem(i.ToString());
+                        lvi.SubItems.Add("Waiting");
+                        lvi.SubItems.Add("");
+                        lvi.SubItems.Add("0");
+                        lv_info.Items.Add(lvi);
+
+                        philosophers[i].start((double)nud_Delay.Value);
+
+                    }
+
+                    break;
+
+                case "Stop":
+
+                    btn_Start.Text = "Start";
+
+                    for (int i = 0; i < numPhils; i++)
+                        philosophers[i].stop(false);
+
+                    break;
+
+            }
+
+        }
+
+        private void interrupt(string single)
+        {
+            switch (single)
+            {
+                case "Start":
+                    lv_info.Items.Clear();
+                    btn_Start.Text = "Stop";
+
+                    numPhils = (int)nud_numPhils.Value;
+                    forks = new Fork[numPhils];
+                    philosophers = new Philosopher[numPhils];
+
+                    //create forks
+                    for (int i = 0; i < numPhils; i++)
+                        forks[i] = new Fork(i);
+
+
+                    //create philosophers and start dining
+                    for (int i = 0; i < numPhils; i++)
+                    {
+                        int rightforkID = i - 1;
+                        if (i == 0)
+                            rightforkID = numPhils - 1;
+
+                        philosophers[i] = new Philosopher(i, forks[i], forks[rightforkID], ref lv_info);
+
+                        ListViewItem lvi = new ListViewItem(i.ToString());
+                        lvi.SubItems.Add("Waiting");
+                        lvi.SubItems.Add("");
+                        lvi.SubItems.Add("0");
+                        lv_info.Items.Add(lvi);
+
+                        philosophers[i].start((double)nud_Delay.Value);
+
+                    }
+
+                    break;
+
+                case "Stop":
+
+                    btn_Start.Text = "Start";
+
+                    for (int i = 0; i < numPhils; i++)
+                        philosophers[i].stop(false);
+
+                    break;
+
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            for (int i = 0; i < numPhils; i++)
+                philosophers[i].stop(true);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control)//用户是否按下了Ctrl键
+            {
+                if (e.KeyCode == Keys.M)
+                {
+                    //用户按下了Ctrl+C.
+                    MessageBox.Show("Restart!");
+                    interrupt("Stop");
+                    interrupt("Start");
+                }
+            }
+        }
+
+
+
+    }
+
+    class Fork
+    {
+        public int forkID;
+        public Philosopher holder = null;
+
+        public Fork(int ID)
+        {
+            forkID = ID;
+        }
+
+        public void RequestFork(Philosopher phil)
+        {
+            if (holder == null || (holder.philID > phil.philID && holder.eating == false))
+            {
+                holder = phil;
+            }
+            else
+            {
+                while (holder != null)
+                    Thread.Sleep(0);
+
+                holder = phil;
+            }
+        }
+
+        public void CleanFork()
+        {
+            holder = null;
+        }
+    }
+
+    delegate void UpdateLV_D(String s, int Row, int SubItem, ref ListView LV);
+
+    class Philosopher
+    {
+        //for UI purposes
+        private ListView LV;
+        UpdateLV_D updateLV = UpdateListView;
+
+        private int pMeals = 0;
+        int numMeals
+        {
+            get { return pMeals; }
+            set
+            {
+                pMeals = value;
+                UpdateListView(value.ToString(), philID, 3, ref LV);
+            }
+        }
+
+        private String pStatus = "";
+        String Status
+        {
+            get { return pStatus; }
+            set
+            {
+                pStatus = value;
+                UpdateListView(value.ToString(), philID, 1, ref LV);
+            }
+        }
+
+        private String pForkStatus = "";
+        String ForkStatus
+        {
+            get { return pForkStatus; }
+            set
+            {
+                pForkStatus = value;
+                UpdateListView(value.ToString(), philID, 2, ref LV);
+            }
+        }
+
+        static void UpdateListView(String s, int Row, int SubItem, ref ListView LV)
+        {
+            if (LV.InvokeRequired)
+            {
+                UpdateLV_D d = new UpdateLV_D(UpdateListView);
+                LV.Invoke(d, new object[] { s, Row, SubItem, LV });
+            }
+            else
+            {
+                LV.Items[Row].SubItems[SubItem].Text = s;
+            }
+        }
+        //
+
+        public bool eating;
+        public Thread pThread;
+        public int philID;
+        private Fork leftFork;
+        private Fork rightFork;
+        private double pDelay = 1.0;
+        private bool dining = false;
+
+        public Philosopher(int ID, Fork lFork, Fork rFork, ref ListView infoLV)
+        {
+            philID = ID;
+            leftFork = lFork;
+            rightFork = rFork;
+
+            pThread = new Thread(new ThreadStart(Dine));
+
+            LV = infoLV;
+        }
+
+        public void start(double delay)
+        {
+            pDelay = delay;
+            dining = true;
+            pThread.Start();
+        }
+
+        public void stop(bool force)
+        {
+            if (force)
+            {
+                pThread.Interrupt();
+            }
+            else
+                dining = false;
+        }
+
+        private void Dine()
+        {
+            try
+            {
+                while (dining)
+                {
+
+                    Status = "Waiting";
+                    {
+                        Thread.Sleep(new Random(DateTime.Now.Millisecond).Next(0, (int)(500 * pDelay)));
+                        if (leftFork.holder != this&&leftFork.holder !=null)
+                        {
+                            //ForkStatus = "LeftFork: " + leftFork.forkID;
+                            ForkStatus = "LeftFork " + leftFork.forkID +" holder: "+ leftFork.holder.philID;
+                            leftFork.RequestFork(this);
+                        }
+                        ForkStatus = "HoldFork: " + leftFork.forkID;
+
+                        Thread.Sleep(new Random(DateTime.Now.Millisecond).Next(0, (int)(500 * pDelay)));
+                        if (rightFork.holder != this)
+                        {
+                            ForkStatus = "RightFork: " + rightFork.forkID + " " + ForkStatus;
+                            rightFork.RequestFork(this);
+                        }
+                        ForkStatus = "HoldFork: " + leftFork.forkID + ", " + rightFork.forkID;
+                    }
+
+                    Status = "Eating";
+                    {
+                        eating = true;
+                        Thread.Sleep(new Random(DateTime.Now.Millisecond).Next(0, (int)(500 * pDelay)));
+                        numMeals += 1;
+                        eating = false;
+                    }
+
+                    Status = "Putting";
+                    {
+                        leftFork.CleanFork();
+                        ForkStatus = "HoldFork: " + rightFork.forkID;
+                        Thread.Sleep(new Random(DateTime.Now.Millisecond).Next(0, (int)(500 * pDelay)));
+                        //rightFork.CleanFork();
+                        ForkStatus = "";
+                        Thread.Sleep(new Random(DateTime.Now.Millisecond).Next(0, (int)(500 * pDelay)));
+                    }
+
+                    Status = "Thinking";
+                    {
+                        Thread.Sleep(new Random(DateTime.Now.Millisecond).Next(0, (int)(500 * (pDelay + 1))));
+                    }
+                }
+            }
+            catch { }
+        }
+
+    }
+}
